948f06e7956c27432d34bef0e8cf9ea5546a4e16
HeidongH <qkqajfl007@naver.com>
2022-04-10 16:57:35 +0900

    test

--- algorithm_Lv01/src/algorithm_Lv01/P_01.java
+++ algorithm_Lv01/src/algorithm_Lv01/P_01.java
@@ -1,11 +1,30 @@
 package algorithm_Lv01;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class P_01 {
 
 	public static void main(String[] args) throws Exception {
 		// 프로그래머스 직사각형 별찍기
 
-		System.out.println("test");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println("n 정수 입력");
+		int n = Integer.parseInt(br.readLine());
+		
+		System.out.println("m 정수 입력");
+		int m = Integer.parseInt(br.readLine());
+
+		for(int i=0; i<m; i++) {
+			for(int j=0; j<n; j++) {
+				System.out.print("*");
+			}
+
+			System.out.println();
+		}
+		
+		br.close();
 	}
 
 }
