4e4992f351b314b7ef2365944e54a705166c0526
HeidongH <qkqajfl007@naver.com>
2022-04-10 16:47:53 +0900

    test - vscode

--- algorithm_Lv01/src/algorithm_Lv01/P_01.java
+++ algorithm_Lv01/src/algorithm_Lv01/P_01.java
@@ -2,8 +2,9 @@ package algorithm_Lv01;
 
 public class P_01 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		// 프로그래머스 직사각형 별찍기
+
 		System.out.println("test");
 	}
 
