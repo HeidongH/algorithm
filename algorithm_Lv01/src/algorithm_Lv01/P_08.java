79c48746faf91a3f1deea83b616e3841bd9c9077
HeidongH <qkqajfl007@naver.com>
2022-04-18 21:33:20 +0900

    피보나치 수

---
+++ algorithm_Lv01/src/algorithm_Lv01/P_08.java
@@ -0,0 +1,27 @@
+package algorithm_Lv01;
+
+public class P_08 {
+    // 피보나치 수
+    public static int sol(int n) {
+        int ans = 0;
+        int a = 1;
+        int b = 1;
+
+        if(n == 1 || n == 2) {
+            return 1;
+        }
+
+        for(int i=3; i<=n; i++) {
+            ans = (a+b) % 1234567;
+            a = b;
+            b = ans;
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int x = sol(5);
+        System.out.println(x);
+    }
+}
