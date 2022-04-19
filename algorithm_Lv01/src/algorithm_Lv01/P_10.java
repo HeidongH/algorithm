7b0ee5ca12aa227344f37da8825375c26bf08621
HeidongH <qkqajfl007@naver.com>
2022-04-19 20:26:14 +0900

    최대공약수와 최소공배수

---
+++ algorithm_Lv01/src/algorithm_Lv01/P_10.java
@@ -0,0 +1,14 @@
+package algorithm_Lv01;
+
+public class P_10 {
+    // 최대공약수와 최소공배수
+    public static int[] sol(int n, int m) {
+        int[] ans = new int[2];
+        
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
