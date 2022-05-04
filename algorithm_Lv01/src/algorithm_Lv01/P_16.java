616658b231c4e5e0bd46840a6ed8ae42301fddb7
HeidongH <qkqajfl007@naver.com>
2022-05-05 00:19:26 +0900

    문자열 전체의 짝, 홀수 인덱스 판별로 해결 / 문제의 해결 제시 방안과는 다름 수정 필요

--- algorithm_Lv01/src/algorithm_Lv01/P_16.java
+++ algorithm_Lv01/src/algorithm_Lv01/P_16.java
@@ -3,29 +3,27 @@ package algorithm_Lv01;
 public class P_16 {
     public static String sol(String s) {
         String ans = "";
-        String[] x = s.split(" ");
-
-        for(int i=0; i<x.length; i++) { // 전체 배열 돌기
-            for(int j=0; j<x[i].length(); j++) { // 배열 원소 하나 글자갯수에서 돌기
-                if(j % 2 == 0) {
-                    // 짝수인 경우
-                    x[i].substring(j, j+1).toUpperCase();
-                }
-                
-            }
+        String[] x = s.split("");
+        
+        for(int i=0; i<x.length; i++) {
+        	x[i] = i%2==0 ? x[i].toUpperCase() : x[i];
         }
+              
         StringBuilder sb = new StringBuilder();
         for(String z : x) {
             sb.append(z);
+            // sb.append(" ");
         }
         ans = sb.toString();
         return ans;
     }
 
     public static void main(String[] args) {
-        String test = "my name is hyeok";
+        String test = "try hello world a b c d e f";
 
         String ans = sol(test);
         System.out.println(ans);
     }
 }
+
+// 문자열 전체의 짝, 홀수 인덱스가 아니라 단어(공백) 기준별로 짝, 홀수 인덱스 판단 해야함.
