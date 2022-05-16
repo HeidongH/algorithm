06d6627b570018de5813adec7142fb23d00348ac
Hyuk <Hyuk@DESKTOP-ADM76FR>
2022-05-16 23:38:48 +0900

    시저 암호

---
+++ algorithm_Lv01/src/algorithm_Lv01/P_18.java
@@ -0,0 +1,22 @@
+package algorithm_Lv01;
+
+public class P_18 {
+	
+	public String sol(String s, int n) {
+		// 시저 암호
+		String ans = "";
+		
+		return ans;
+	}
+
+	public static void main(String[] args) {
+		P_18 p = new P_18();
+		
+		String s = "";
+		int n = 0;
+		
+		System.out.println(p.sol(s, n));
+
+	}
+
+}
