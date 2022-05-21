75cda1cfaf623c75be993228e5ba702d116222f3
Hyuk <Hyuk@DESKTOP-ADM76FR>
2022-05-21 19:14:13 +0900

    시저 암호

--- algorithm_Lv01/src/algorithm_Lv01/P_18.java
+++ algorithm_Lv01/src/algorithm_Lv01/P_18.java
@@ -5,30 +5,32 @@ import java.util.Arrays;
 public class P_18 {
 	
 	public String sol(String s, int n) {
-		// ì‹œì € ì•”í˜¸
+		// ½ÃÀú ¾ÏÈ£
 		String ans = "";
 		String [] r = s.split("");
 		
-		char [] upper = new char[26];
-		char [] lower = new char[26];
+		// 65 ~ 90 Upper
+		// 97 ~ 122 Lower
 		
+		char x = 65;
 		int start = 0;
-		for(char i='A'; i<='Z'; i++) {
-			for(int j=0; j<r.length; j++) {
-				if(r[j].equals(String.valueOf(i))) {
-					ans += Character.toString((char) (i+1));
-				} else if(r[j].equals(" ")) {
-					ans += r[j];
-				}
+		while(x <= 122) {
+			if(start >= r.length) break;
+			
+			if(r[start].equals(Character.toString(x))) {
+				/*if(r[start].equals("Z")) ans += "A";
+				else if(x == 122) ans += 97;*/
+				ans += Character.toString((char) (x+n));
+				start++;
+				continue;
+			} else if(r[start].equals(" ")) {
+				ans += r[start]; 
+				start++;
+				continue;
 			}
 			
-			start++;
-		}
-		
-		start = 0;
-		for(char i='a'; i<='z'; i++) {
-			lower[start] = i;
-			start++;
+			x++;
+			
 		}
 		
 		System.out.println(Arrays.toString(r));
@@ -38,8 +40,8 @@ public class P_18 {
 	public static void main(String[] args) {
 		P_18 p = new P_18();
 		
-		String s = "Z";
-		int n = 0;
+		String s = "AB C d z Z";
+		int n = 1;
 		
 		System.out.println(p.sol(s, n));
 		
