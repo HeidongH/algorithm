9790514be9d5e4a557175d63065b5d9eea88a7d6
HeidongH <qkqajfl007@naver.com>
2022-05-23 23:56:47 +0900

    Test 추가

---
+++ algorithm_Lv01/src/algorithm_Lv01/Test.java
@@ -0,0 +1,17 @@
+package algorithm_Lv01;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String ans = "";
+		
+		String s = "Hello";
+		char ch = s.charAt(1);
+		
+		System.out.println(ch);
+
+	}
+
+}
