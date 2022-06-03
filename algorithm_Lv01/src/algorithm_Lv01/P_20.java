2ccff8028241ca28bbca784c03c077f8fa9d8071
nebula0225 <qkqajfl007@naver.com>
2022-06-03 16:00:40 +0900

    수박수박

---
+++ algorithm_Lv01/src/algorithm_Lv01/P_20.java
@@ -0,0 +1,24 @@
+package algorithm_Lv01;
+
+class P_20 {
+    public String solution(int n) {
+        String answer = "";
+
+        for(int i=1; i<=n; i++) {
+            if(i % 2 == 0) { 
+                answer += "박";
+            } else {
+                answer += "수";
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        P_20 sol = new P_20();
+
+        System.out.println(sol.solution(5));
+
+    }
+}
