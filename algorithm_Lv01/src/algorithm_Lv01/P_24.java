73ca2b9ac77d12848f917b72955e4330d3e86c89
HeidongH <qkqajfl007@naver.com>
2022-06-08 01:23:48 +0900

    문자열 내림차순으로 배치하기

--- algorithm_Lv01/src/algorithm_Lv01/P_24.java
+++ algorithm_Lv01/src/algorithm_Lv01/P_24.java
@@ -1,17 +1,24 @@
 package algorithm_Lv01;
 
+import java.util.Arrays;
+
 public class P_24 {
 	
 	public String sol(String s) {
-		String ans = "";
+		// 65 ~ 90 Upper 대문자
+		// 97 ~ 122 Lower 소문자
+
+		char[] x = s.toCharArray();
 		
-		String[] x = s.split("");
+		// char로 형변환 했기 때문에 아스키 코드값 순으로 정렬
+		Arrays.sort(x); 
 		
+		// System.out.println(x);
 		
-		
-		
-		
-		return ans;
+		// char 배열을 String으로, 역순 정렬을 하기위해서 StringBuilder 사용
+		StringBuilder sb = new StringBuilder(new String(x));
+		return sb.reverse().toString();
+
 	}
 
 	public static void main(String[] args) {
--- algorithm_Lv01/src/algorithm_Lv01/Test.java
+++ algorithm_Lv01/src/algorithm_Lv01/Test.java
@@ -1,17 +1,31 @@
 package algorithm_Lv01;
 
+import java.util.Arrays;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		// 65 ~ 90 Upper 대문자
+		// 97 ~ 122 Lower 소문자
+		
 		String ans = "";
-		
-		String s = "Hello";
-		char ch = s.charAt(1);
-		
-		System.out.println(ch);
+		String s = "ZbcdefgA";
 
+		char[] x = s.toCharArray();
+		
+		// char로 형변환 했기 때문에 아스키 코드값 순으로 정렬
+		Arrays.sort(x); 
+		
+		// System.out.println(x);
+		
+		// char 배열을 String으로, 역순 정렬을 하기위해서 StringBuilder 사용
+		StringBuilder sb = new StringBuilder(new String(x));
+		sb.reverse().toString();
+		
+		// System.out.println(sb);
+		
 	}
 
 }
