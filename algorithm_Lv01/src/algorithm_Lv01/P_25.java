df28273b3898b91bb96bbd90a60e42bf3aa0e314
nebula0225 <qkqajfl007@naver.com>
2022-06-09 14:03:19 +0900

    문자열 내 마음대로 정렬하기 - 완

--- algorithm_Lv01/src/algorithm_Lv01/P_25.java
+++ algorithm_Lv01/src/algorithm_Lv01/P_25.java
@@ -5,16 +5,16 @@ import java.util.Arrays;
 public class P_25 {
 	
 	public String[] solution(String[] strings, int n) {
-        String[] answer = {};
+        String[] answer = new String[strings.length];
         
-        for(int i=0; i<strings.length; i++) { // "sun"
+        for(int i=0; i<strings.length; i++) { 
         	answer[i] = String.valueOf(strings[i].charAt(n)) + strings[i];
         }
         
         Arrays.sort(answer);
         
         for(int i = 0; i<strings.length; i++) {
-        	answer[i] = answer[i].substring(1, answer[i].length()-1);
+        	answer[i] = answer[i].substring(1, answer[i].length());
         }
         
         return answer;
@@ -23,10 +23,10 @@ public class P_25 {
 	public static void main(String[] args) {
 		// 문자열 내 마음대로 정렬하기
 		P_25 p = new P_25();
-		String [] x = {"sun", "bed", "car"};
-		int n = 1;
+		String [] x = {"abce", "abcd", "cdx"};
+		int n = 2;
 		
-		System.out.println(p.solution(x, n));
+		System.out.println(Arrays.toString(p.solution(x, n)));
 		
 	}
 
