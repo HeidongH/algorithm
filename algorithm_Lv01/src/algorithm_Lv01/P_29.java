f9037ce16345b3fa34b96c9dc1881e118d45dfbf
nebula0225 <qkqajfl007@naver.com>
2022-06-14 14:29:04 +0900

    같은 숫자는 싫어

--- algorithm_Lv01/src/algorithm_Lv01/P_29.java
+++ algorithm_Lv01/src/algorithm_Lv01/P_29.java
@@ -1,10 +1,37 @@
 package algorithm_Lv01;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class P_29 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public int[] solution(int []arr) {
 
+		List<Integer> ans = new ArrayList<>();
+		int cnt = 0;
+
+		ans.add(arr[0]);
+
+		for(int i = 0; i<arr.length; i++) {
+			if(i+1 == arr.length) break;
+
+			if(ans.get(cnt) == arr[i+1]) {
+				continue;
+			}
+			ans.add(arr[i+1]);
+			cnt++;
+		}
+
+		return ans.stream().mapToInt(x -> x).toArray();
+    }
+
+	public static void main(String[] args) {
+		// 같은 숫자는 싫어
+		P_29 p = new P_29();
+		int[]arr = {4,4,4,3,3};
+		int[]ans = p.solution(arr);
+		System.out.println(Arrays.toString(ans));
 	}
 
 }
