29f5ec0abfc2a11c6fd137672d2fd7c5a81532af
HeidongH <qkqajfl007@naver.com>
2022-06-24 01:13:08 +0900

    다트 게임

--- algorithm_Lv01/src/algorithm_Lv01/P_30.java
+++ algorithm_Lv01/src/algorithm_Lv01/P_30.java
@@ -1,25 +1,31 @@
 package algorithm_Lv01;
 
-import java.util.List;
-
 public class P_30 {
+	
+	public static Double check(String x) {
+		
+		double cnt = 0;
+		
+		if(x.equals("S")) {
+			cnt = 1;
+		} else if(x.equals("D")) {
+			cnt = 2;
+		} else if(x.equals("T")) {
+			cnt = 3;
+		}
+		
+		return cnt;
+	}
 
     public int solution(String dartResult) {
         // 다트 게임
-        List<Integer> cal = null;
-
         int answer = 0;
+        int cnt = 0;
 
         String [] arr = dartResult.split("");
 
-        for(int i=1; i<arr.length; i++) {
-            if(arr[i].equals("S") && !arr[i+1].equals("*") && !arr[i+1].equals("#")) {
-                answer += (Integer.parseInt(arr[i-1]));
-            } else if(arr[i].equals("D") && !arr[i+1].equals("*") && !arr[i+1].equals("#")) {
-                answer += Math.pow(Double.parseDouble(arr[i-1]), 2);
-            } else if(arr[i].equals("T") && !arr[i+1].equals("*") && !arr[i+1].equals("#")) {
-                answer += Math.pow(Double.parseDouble(arr[i-1]), 3);
-            } 
+        for(int i=0; i<arr.length; i++) {
+        	
         }
 
 
@@ -28,7 +34,7 @@ public class P_30 {
     
     public static void main(String[] args) {
         P_30 p = new P_30();
-
+        
         String x = "1S2D*3T";
 
         System.out.println(p.solution(x));
