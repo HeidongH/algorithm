0ba5382877d73463bbf601cba64b5d5ce2b4c704
최광혁 <gh.choi@delcam.co.kr>
2022-06-29 18:00:26 +0900

    test

--- algorithm_Lv01/src/algorithm_Lv01/Test.java
+++ algorithm_Lv01/src/algorithm_Lv01/Test.java
@@ -1,30 +1,15 @@
 package algorithm_Lv01;
 
-import java.util.Arrays;
-
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		// 65 ~ 90 Upper 대문자
-		// 97 ~ 122 Lower 소문자
-		
-		String ans = "";
-		String s = "ZbcdefgA";
+		int[][] sizes = {{60,50}, {30,70}, {60,30}, {80,40}};
 
-		char[] x = s.toCharArray();
-		
-		// char로 형변환 했기 때문에 아스키 코드값 순으로 정렬
-		Arrays.sort(x); 
-		
-		// System.out.println(x);
-		
-		// char 배열을 String으로, 역순 정렬을 하기위해서 StringBuilder 사용
-		StringBuilder sb = new StringBuilder(new String(x));
-		sb.reverse().toString();
-		
-		// System.out.println(sb);
+		for(int i=0; i<sizes.length; i++) {
+            for(int j=0; j<sizes[i].length; j++) {
+				System.out.println(sizes[i][j]);
+			}
+        }
 		
 	}
 
