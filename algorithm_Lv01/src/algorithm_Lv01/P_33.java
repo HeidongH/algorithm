ea0665f6d7e9dd66e72a32f7aaed8f36372eff47
최광혁 <gh.choi@delcam.co.kr>
2022-06-30 15:02:14 +0900

    최소직사각형

--- algorithm_Lv01/src/algorithm_Lv01/P_33.java
+++ algorithm_Lv01/src/algorithm_Lv01/P_33.java
@@ -1,34 +1,37 @@
 package algorithm_Lv01;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class P_33 {
     
     public int solution(int[][] sizes) {
         // 최소직사각형
-        int answer = 0;
-        int a = 0;
-        int b = 0;
-        int[][] check = new int[a][b];
 
-        // for(int i=0; i<sizes.length; i++) {
-        //     if(sizes[i][0] > a) { // 2차원 배열의 첫번째 원소 탐색
-        //         a = sizes[i][0];
-        //     }
-        //     if(sizes[i][1] > b) { // 2차원 배열의 두번째 원소 탐색
-        //         b = sizes[i][1];
-        //     }
-        // }
+        int a = 0; // 가로
+        int b = 0; // 세로
+
+        List<Integer> width = new ArrayList<>();
+        List<Integer> height = new ArrayList<>();
 
         for(int i=0; i<sizes.length; i++) {
-            if(sizes[i][0] > a) {
-                if(sizes[i][1] > a) {
-                    
+            for(int j=0; j<sizes[i].length; j++) {
+                if(sizes[i][j] >= sizes[i][j+1]) {
+                    width.add(sizes[i][j]);
+                    height.add(sizes[i][j+1]);
+                } else {
+                    width.add(sizes[i][j+1]);
+                    height.add(sizes[i][j]);
                 }
+                j++; // 다음 방으로
             }
         }
 
-        System.out.println(a +""+ b);
+        a = Collections.max(width);
+        b = Collections.max(height);
 
-        return answer;
+        return a * b;
     }
 
     public static void main(String[] args) {
@@ -36,6 +39,6 @@ public class P_33 {
         int[][] sizes = {{60,50}, {30,70}, {60,30}, {80,40}};
 
         p.solution(sizes);
-        // System.out.println(p.solution(sizes));
+        System.out.println(p.solution(sizes));
     }
 }
