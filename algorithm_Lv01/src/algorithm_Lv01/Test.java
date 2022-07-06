f621f28beeefcf5f3a73005c114aa68ebd96841c
HeidongH <qkqajfl007@naver.com>
2022-07-06 23:45:19 +0900

    비밀 지도 - 다른사람 풀이 분석

--- algorithm_Lv01/src/algorithm_Lv01/Test.java
+++ algorithm_Lv01/src/algorithm_Lv01/Test.java
@@ -1,5 +1,6 @@
 package algorithm_Lv01;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 public class Test {
@@ -37,16 +38,34 @@ public class Test {
 
 		return list;
 	}
+	
+	public static String[] solution(int n, int[] arr1, int[] arr2) {
+		// 다른 사람 풀이
+        String[] result = new String[n];
+        
+        for (int i = 0; i < n; i++) {
+            result[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            result[i] = String.format("%" + n + "s", result[i]);
+            result[i] = result[i].replaceAll("1", "#");
+            result[i] = result[i].replaceAll("0", " ");
+        }
+
+        return result;
+    }
 
 	public static void main(String[] args) {
 
 		int x = 21;
 		int n = 6;
 		
-		Test t = new Test();
-		for(String s : t.binary(x, n)) {
-			System.out.println(s);
-		}
+		int[] arr1 = {46, 33, 33 ,22, 31, 50};
+		int[] arr2 = {27 ,56, 19, 14, 14, 10};
+		
+		System.out.println(Arrays.toString(solution(n, arr1, arr2)));
+		
 
 	}
 
