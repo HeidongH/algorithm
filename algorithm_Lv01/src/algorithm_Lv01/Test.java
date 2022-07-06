990b459e6f0d8c3bc9b224f4986fb02652bfbe10
최광혁 <gh.choi@delcam.co.kr>
2022-07-06 15:34:22 +0900

    test

--- algorithm_Lv01/src/algorithm_Lv01/Test.java
+++ algorithm_Lv01/src/algorithm_Lv01/Test.java
@@ -1,24 +1,40 @@
 package algorithm_Lv01;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Stack;
 
 public class Test {
 
 	public static void main(String[] args) {
 
-		List<Integer> list = new ArrayList<>();
+		int x = 1;
+		int n = 5;
 
-		list.add(1);
-		list.add(2);
-		list.add(3);
-		list.add(4);
-		list.add(5);
+		Stack<String> stack = new Stack<>();
+		String[] list = new String[n]; // 2진수 저장 배열 (방크기는 한변의 길이 n)
 
-		list.forEach(x -> System.out.println(x)); // 출력
-		list.stream().forEach(x -> System.out.println(x)); // 출력
-		System.out.println();
-		list.stream().map(x -> x * 2).forEach(x -> System.out.println(x));
+		for(int i=x; i>=1; i=i/2) { // 10진수 -> 2진수로 변환
+			if(i%2 == 1) {
+				stack.push("1");
+			} else if(i%2 == 0) {
+				stack.push("0");
+			}
+		}
+
+		int check = 0;
+		if(stack.size() != n) {
+			for(int i=0; i<n - stack.size(); i++) {
+				list[i] = "0";
+				check = i;
+			}
+		}
+
+		while(!stack.empty()) { // stack에서 위에서부터 꺼내면서 출력
+			list[check+1] = stack.pop();
+		}
+
+		for(String s : list) {
+			System.out.println(s);
+		}
 		
 		
 
