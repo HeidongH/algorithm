714103bc54bf897e3258574e473832b1281e5cfb
최광혁 <qkqajfl007@naver.com>
2022-07-14 16:54:39 +0900

    예산 - 틀 추가

---
+++ algorithm_Lv01/src/algorithm_Lv01/P_38.java
@@ -0,0 +1,22 @@
+package algorithm_Lv01;
+
+public class P_38 {
+    
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+
+        
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        // 예산
+
+        P_38 p = new P_38();
+        int[] d = {1,3,2,5,4};
+        int budget = 9;
+
+        System.out.println(p.solution(d, budget));
+    }
+}
