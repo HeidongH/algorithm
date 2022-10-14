4b5c4fa8d71c9302899fbce2b0b3d49bb396498d
nebula0225 <qkqajfl007@naver.com>
2022-10-15 02:40:17 +0900

    삼총사 - 완

--- algorithm_Lv01/src/algorithm_Lv01/P_43.java
+++ algorithm_Lv01/src/algorithm_Lv01/P_43.java
@@ -2,26 +2,39 @@ package algorithm_Lv01;
 
 public class P_43 {
 	
-	public int[] solution(int[] lottos, int[] win_nums) {
-        int[] answer = {};
-        
-        
-        
-        
-        return answer;
-    }
+	public static int ans = 0;
+	
+	public int solution(int[] number) {	
+		int[] list = new int[number.length-1];
+		
+		if(number.length < 3) { // 더이상 더할 수가 없을 경우 탈출
+			return ans;
+		}
+		
+		for(int i=0; i<number.length; i++) {
+			if(i+1 != number.length) {
+				list[i] = number[i+1];
+			}
+			for(int j=i+2; j<number.length; j++) {
+				if(i == number.length) {
+					break;
+				}
+				int x = number[0] + number[i+1] + number[j];
+				if(x == 0) {
+					ans++;
+				}
+			}
+		}
+		return solution(list);
+	}
 
 	public static void main(String[] args) {
-		// 로또의 최고 순위와 최저 순위
+		// 삼총사
 		P_43 p = new P_43();
-		int[] lottos = {44, 1, 0, 0, 31, 25};
-		int[] win_nums = {31, 10, 45, 1, 6, 19};
+		int[] number = {-3, -2, -1, 0, 1, 2, 3};
 		
-		int[] ans = p.solution(lottos, win_nums);
+		System.out.println(p.solution(number));
 		
-		for(int x : ans) {
-			System.out.println(x);
-		}
 	}
 
 }
