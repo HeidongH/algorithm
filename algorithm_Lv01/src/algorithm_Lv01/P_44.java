6d84c9ed5155bf0896af2965ab7e7d46320758fc
user <user@DESKTOP-choikwanghyeok>
2022-11-04 10:46:45 +0900

    콜라 문제 - 다른 풀이 방법 추가

--- algorithm_Lv01/src/algorithm_Lv01/P_44.java
+++ algorithm_Lv01/src/algorithm_Lv01/P_44.java
@@ -17,17 +17,36 @@ public class P_44 {
         
         return solution(a, b, empty);
     }
+	
+	public int other(int a, int b, int n) {
+		/*
+		 * a = 3
+		 * b = 2
+		 * n = 20
+		 * ans = 36
+		 */
+		
+		int ans = 0;
+		while(n >= a) { // n이 크거나 같을동안 반복 실행
+			ans += n/a*b;
+			n = n%a + n/a*b;
+		}
+		return ans;
+	}
 
 	public static void main(String[] args) {
-		// 콜라문제
+		// 콜라 문제
+		
 		int a = 3;
 		int b = 2;
 		int n = 20;
 		
 		P_44 p = new P_44();
 		int result = p.solution(a, b, n);
-		
 		System.out.println(result);
+		
+		int result2 = p.other(a, b, n);
+		System.out.println(result2);
 	}
 
 }
